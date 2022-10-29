2862f11a232520d69cce1ab3cbe2b841ef1252cd
Adrian-Larenaudie <adrian.larenaudie@gmail.com>
2022-10-29 14:03:09 +0200

    WIP step 2: draw_snake_head(), msg: draw snake head ok;

--- js/jormungand.js
+++ js/jormungand.js
@@ -1,10 +1,12 @@
 //? La fonction principale du jeu:
 
 import { draw_grid } from './utils/draw_grid.js';
+import { draw_snake_head } from './utils/draw_snake_head.js';
 
 const jormungand = () => {
     console.log('hello world');
     draw_grid();
+    draw_snake_head();
 };
 
 jormungand();
--- js/utils/draw_snake_head.js
+++ js/utils/draw_snake_head.js
@@ -2,11 +2,20 @@ export const draw_snake_head = () => {
     // pour faire simple on va juste déssiner un carré vert sur la grille
     const snake = {
         head: {
-            size: 50,
+            size: 40,
             color: "#00561B",
+            position: {
+                // sur l'axe horizontale
+                x: 1,
+                // sur l'axe verticale
+                y: 1,
+            }
         },
     }
     // balise canvas
     const canvas = document.querySelector('canvas');
-
-}
+    // on donne un context au canvas, (le jeu sera en 2d)
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#00561B';
+    ctx.fillRect(snake.head.position.x, snake.head.position.y, snake.head.size, snake.head.size);
+};
