c1a971184a66b902edb10d9abd9dc230940d71af
Adrian-Larenaudie <adrian.larenaudie@gmail.com>
2022-10-29 08:50:38 +0200

    CREATE index.html, styles.css, jormungand.js + DISCRIBE first step (create a grid using canvas)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Jormungand 
+
+// 1ere étape créer une grille de 50 cases par 50 cases sur la balise canvas à l'aide d'une fonction: 
+// 
---
+++ js/jormungand.js
@@ -0,0 +1,5 @@
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 100, 100);
